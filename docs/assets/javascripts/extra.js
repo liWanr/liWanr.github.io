f18049c278905ae4d63c7dc4786c5e45bdef7208
liWanr <itsWanr@outlook.com>
2026-02-17 19:30:09 +0000

    融合表格排序、公式到extra里面

--- docs/assets/javascripts/extra.js
+++ docs/assets/javascripts/extra.js
@@ -1,6 +1,6 @@
 // 文章中以新标签页打开外部链接
-document.addEventListener('DOMContentLoaded', function() {
-    const content = document.querySelector('.md-typeset');
+document$.subscribe(function() {
+    const content = document.querySelector(".md-typeset");
     if (!content) return;
 
     content.querySelectorAll('a[href^="http"], a[href^="https"]').forEach(link => {
@@ -10,3 +10,32 @@ document.addEventListener('DOMContentLoaded', function() {
         link.setAttribute('rel', 'noopener noreferrer');
     });
 });
+
+// 表格排序
+// document$.subscribe(function() {
+//   var tables = document.querySelectorAll("article table:not([class])")
+//   tables.forEach(function(table) {
+//     new Tablesort(table)
+//   })
+// })
+
+// 数学公式
+// window.MathJax = {
+//   tex: {
+//     inlineMath: [["\\(", "\\)"]],
+//     displayMath: [["\\[", "\\]"]],
+//     processEscapes: true,
+//     processEnvironments: true
+//   },
+//   options: {
+//     ignoreHtmlClass: ".*|",
+//     processHtmlClass: "arithmatex"
+//   }
+// };
+
+// document$.subscribe(() => { 
+//   MathJax.startup.output.clearCache()
+//   MathJax.typesetClear()
+//   MathJax.texReset()
+//   MathJax.typesetPromise()
+// })
