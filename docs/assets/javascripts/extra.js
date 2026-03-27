2257fc4d4ceecdbde02b02757f4a95cef111e092
liWanr <li.Wanr@outlook.com>
2026-03-27 20:43:50 +0000

    移除MathJax公式注释并整理代码结构

--- docs/assets/javascripts/extra.js
+++ docs/assets/javascripts/extra.js
@@ -11,31 +11,23 @@ document$.subscribe(function() {
     });
 });
 
-// 表格排序
-// document$.subscribe(function() {
-//   var tables = document.querySelectorAll("article table:not([class])")
-//   tables.forEach(function(table) {
-//     new Tablesort(table)
-//   })
-// })
-
 // 数学公式
-// window.MathJax = {
-//   tex: {
-//     inlineMath: [["\\(", "\\)"]],
-//     displayMath: [["\\[", "\\]"]],
-//     processEscapes: true,
-//     processEnvironments: true
-//   },
-//   options: {
-//     ignoreHtmlClass: ".*|",
-//     processHtmlClass: "arithmatex"
-//   }
-// };
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
 
-// document$.subscribe(() => { 
-//   MathJax.startup.output.clearCache()
-//   MathJax.typesetClear()
-//   MathJax.texReset()
-//   MathJax.typesetPromise()
-// })
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
