cabd31207f58d3072c9d0ca6375e10cabfaae8ec
liWanr <li.Wanr@outlook.com>
2026-04-04 21:41:36 +0000

    移除注释，简化代码，提升可读性

--- docs/assets/javascripts/extra.js
+++ docs/assets/javascripts/extra.js
@@ -1,8 +1,6 @@
-// 文章中以新标签页打开外部链接
 document$.subscribe(function() {
     const content = document.querySelector(".md-typeset");
     if (!content) return;
-
     content.querySelectorAll('a[href^="http"], a[href^="https"]').forEach(link => {
         if (link.href.startsWith(window.location.origin)) return;
 
